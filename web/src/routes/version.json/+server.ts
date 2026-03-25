64b378fb38d84287e1a8004aa4d6217a1863bfbe
Tommy Green <91552725+daqiang789@users.noreply.github.com>
2026-03-25 18:04:21 +0800

    Update +server.ts

--- web/src/routes/version.json/+server.ts
+++ web/src/routes/version.json/+server.ts
@@ -2,10 +2,17 @@ import { json } from "@sveltejs/kit";
 import { getCommit, getBranch, getRemote, getVersion } from "@imput/version-info";
 
 export async function GET() {
+    let remoteInfo = "unknown";
+    try {
+        remoteInfo = await getRemote();
+    } catch (e) {
+        console.warn("Vercel env: skipped remote parsing");
+    }
+
     return json({
         commit: await getCommit(),
         branch: await getBranch(),
-        remote: await getRemote(),
+        remote: remoteInfo,
         version: await getVersion()
     });
 }
