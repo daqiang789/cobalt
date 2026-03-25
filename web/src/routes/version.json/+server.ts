e53c0a34bce123ad7df691203dd77b1a97d7a0f1
Tommy Green <91552725+daqiang789@users.noreply.github.com>
2026-03-25 18:06:47 +0800

    Update +server.ts

--- web/src/routes/version.json/+server.ts
+++ web/src/routes/version.json/+server.ts
@@ -1,11 +1,11 @@
 import { json } from "@sveltejs/kit";
-import { getCommit, getBranch, getRemote, getVersion } from "@imput/version-info";
+import { getCommit, getBranch, getVersion } from "@imput/version-info";
 
 export async function GET() {
     return json({
         commit: await getCommit(),
         branch: await getBranch(),
-        remote: await getRemote(),
+        remote: "unknown", // 这里强制写死，不让它去读Git远程信息，解决报错
         version: await getVersion()
     });
 }
